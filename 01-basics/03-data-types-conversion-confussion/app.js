b85302a233699f05cce52a9b26a1426ff1f134ab
Sumesh <sumeshmon2006@gmail.com>
2024-03-08 16:37:52 +0530

    files

---
+++ 01-basics/03-data-types-conversion-confussion/app.js
@@ -0,0 +1,13 @@
+let rollNumber = "sada"
+let converted = Number(rollNumber)
+//console.log(typeof rollNumber)
+//console.table(converted)
+
+
+function test(){
+    let name = false
+    let converted = String(name)
+    console.log(typeof name)
+    console.log(converted)
+}
+test()
