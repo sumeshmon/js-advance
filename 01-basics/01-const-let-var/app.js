13e0f31038b42c57890b92cb05cd8963b087d61f
Sumesh <sumeshmon2006@gmail.com>
2024-03-08 15:33:57 +0530

    files

---
+++ 01-basics/01-const-let-var/app.js
@@ -0,0 +1,15 @@
+const accountID = 123654
+let accountName = "Sumesh"
+var accountEmail = "sumesh.com"
+accountState = "Kerala"
+userName = "sumeshks"
+let accountCountry
+
+
+let test = accountCountry = "ddd"
+
+
+accountName = "Sali"
+accountEmail = "sds"
+
+console.table([accountID,accountName,accountEmail,accountCountry,test,test1])
---
+++ 01-basics/02-data-types/app.js
@@ -0,0 +1,6 @@
+const number = 456
+let name = "sumesh"
+let rollNumber
+let address = false
+
+console.table(["number" + number,name="sali",rollNumber,address])
