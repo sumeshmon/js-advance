7c335963821c054f0fad1e4b80ab39f784326d7a
Sumesh <sumeshmon2006@gmail.com>
2024-03-09 16:25:42 +0530

    data type summary

---
+++ 01-basics/07-memoryUsage.js
@@ -0,0 +1,25 @@
+// Stack (Primitive) and Heap (Non Primitive)
+
+// Stack memories is copy 
+// Heap memory is the referance
+
+// Stack example
+let myCar = "Honda Brio"
+let myNewCar = myCar
+myCar = "BMW"
+console.log(myCar,myNewCar);
+
+
+// Heap
+let userOne = {
+    name : "Sumesh",
+    age : 22
+}
+let usertwo = {
+    name : "Sali",
+    age : 23
+}
+userOne = usertwo
+userOne.name = "Tejas"
+
+console.log(userOne, usertwo);
