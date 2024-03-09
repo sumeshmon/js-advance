c6119ddb051004c6b9877b7055d6c100a43d7c79
Sumesh <sumeshmon2006@gmail.com>
2024-03-09 14:37:26 +0530

    files

--- 01-basics/03-conversionOperator.js
+++ 01-basics/03-conversionOperator.js
@@ -2,8 +2,13 @@ let rollNumber = "sada"
 let converted = Number(rollNumber)
 //console.log(typeof rollNumber)
 //console.table(converted)
-
-
+let numtest = function numberTest(){
+    let num1 = 3
+    let num2 = "6"
+    let sum = num1 + Number(num2)
+    console.log(sum);
+}
+numtest();
 function stringTest(){
     let name = false
     let converted = String(name)
@@ -13,12 +18,13 @@ function stringTest(){
 //stringTest()
 
 function boolenTest(){
-    let name = ""
+    let name = true
     let converted = Boolean(name)
     console.log(typeof name)
-    console.log(converted)
+    console.log(typeof converted)
 }
 //boolenTest()
 
-console.log("2"+2+2);
-console.log(2+2+"2");
+//console.log("2"+2+2);
+//console.log(2+2+"2");
+
