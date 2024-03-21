a3860d0800fbd211671570a66097cf41092a475e
Sumesh <sumeshmon2006@gmail.com>
2024-03-21 09:22:50 +0530

    array

---
+++ 02-bascis/01-array.js
@@ -0,0 +1,22 @@
+const myArray = [0,1,2,3,4,5]
+
+//console.log(myArray.length);
+//myArray.push(100)
+//myArray.pop() // remove the last element of an array
+//myArray.unshift(200) // add an element in the begining of an array
+//myArray.shift() // remove the first element of an array
+
+//console.log(myArray.includes(210)); // check if any element is matching, the return value will be boolen
+//console.log(myArray.indexOf(30)); // to get the position
+//console.log(myArray.findIndex("20"));
+
+console.log("A ", myArray);
+
+const arr1 = myArray.slice(0, 3)
+console.log("Sliced", arr1);
+console.log("B ", myArray);
+
+const arr2 = myArray.splice(0,3)
+console.log("Spliced", arr2);
+console.log("C", myArray);
+
