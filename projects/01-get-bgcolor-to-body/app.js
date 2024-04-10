613d9f83a931173831c07e3e8e266fb7cc20fc41
Sumesh <sumeshmon2006@gmail.com>
2024-04-10 15:44:19 +0530

    project

---
+++ projects/01-get-bgcolor-to-body/app.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.buttons')
+const body = document.querySelector('body')
+
+buttons.forEach( function (span) {
+    // console.log(span);
+    span.addEventListener('click', function(e){
+        console.log(e.target);
+        switch (e.target.id) {
+            case 'blue':
+                body.style.backgroundColor = e.target.id
+                break;
+                
+            case 'green':
+                body.style.backgroundColor = e.target.id
+                break;
+            case 'red':
+                body.style.backgroundColor = e.target.id
+                break;
+           
+            default:
+                body.style.backgroundColor = 'yellow'
+                break;
+        }
+        
+    })
+} )
