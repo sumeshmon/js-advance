195cfa294eee8d0494a438c6c7fb031c7e51cf55
Sumesh <sumeshmon2006@gmail.com>
2024-05-01 07:59:06 +0530

    promise

--- 09-promise/app.js
+++ 09-promise/app.js
@@ -1,17 +1,36 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(function(response) {
-    console.log(response);
-    return response.json()  
-})
-.then((data) =>{
-    console.log(data[2].email);
-})
-.then((data1) =>{
-    //console.log(data1.length);
-})
-.catch(function(error) {
-    console.log('Oops! Something went wrong using .then() and .catch()', error);
-})
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(function(response) {
+//     console.log(response);
+//     return response.json()  
+// })
+// .then((data) =>{
+//     console.log(data[2].email);
+// })
+// .then((data1) =>{
+//     //console.log(data1.length);
+// })
+// .catch(function(error) {
+//     console.log('Oops! Something went wrong using .then() and .catch()', error);
+// })
 // .finally(function() {
 //     console.log('Finally block executed or rejected');
 // });
+
+
+
+
+// +++++++++++++ using async method +++++++++++++++++
+
+async function getDetailsFromApi() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data[2]);
+    } catch (error) {
+        console.log('Oops! Something went wrong using async method', error);
+    }
+   
+}
+
+
+getDetailsFromApi()
