29d018d7e7c65866a07e2b59f5460165b660cce3
Sumesh <sumeshmon2006@gmail.com>
2024-05-03 07:44:59 +0530

    inheritance

--- 11-prototype/prototype.js
+++ 11-prototype/prototype.js
@@ -10,8 +10,35 @@ Array.prototype.subi = function () {
     console.log(`I am subi from array`);
 }
 
-myObj.sumesh()
-myArray.sumesh()
+// myObj.sumesh()
+// myArray.sumesh()
 
 // myObj.subi()
-myArray.subi()
+// myArray.subi()
+
+// ++++++++++++ inheritance ++++++++++++
+
+const user = {
+    name:'sumesh',
+    email:'sumesh@sumesh.com'
+}
+const isTeacher = {
+    videpUpload : true
+}
+const isAssistance = {
+    available: false,
+    __proto__: user
+}
+
+console.log(isAssistance.name); // name is an diiferent object that accessed by __proto__
+console.log(isAssistance.available);
+
+console.log('break');
+
+isTeacher.__proto__ = user
+console.log(isTeacher.videpUpload);
+console.log(isTeacher.email);
+
+// modern method
+Object.setPrototypeOf(isTeacher,user)
+console.log(`I am new method ${isTeacher.email}`);
