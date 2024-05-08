7380498328fde6da75168674048776896a19b480
Sumesh <sumeshmon2006@gmail.com>
2024-05-08 07:46:41 +0530

    getter setter

---
+++ 10-class-OOP/getter-setter.js
@@ -0,0 +1,19 @@
+class School {
+    constructor(name,std,isMale){
+        this.name = name
+        this.std = std
+        this.isMale = isMale
+    }
+
+    get name() {
+        return `${this._name.toUpperCase()}123`
+    }
+
+    set name (val) {
+        this._name = val
+    }
+
+}
+
+const student1 = new School('sumesh',8,'no')
+console.log(student1.name);
