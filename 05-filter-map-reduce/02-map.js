aeeaadb04ff2cc1d11b65eb25ae6ff40cf2593d0
Sumesh <sumeshmon2006@gmail.com>
2024-05-12 09:56:06 +0530

    random color

--- 05-filter-map-reduce/02-map.js
+++ 05-filter-map-reduce/02-map.js
@@ -2,17 +2,17 @@
 
 //Applies a function to each element of an array and returns a new array containing the results.
 
-const myNum = [1,2,3,4,5,6,7,8,9,10]
+const myNum = [7,1,2,3,4]
 
 const multiplyWithTwo = myNum.map( (param) => param * 2 ) 
 // const multiplyWithTwo = myNum.filter((param) => param * 2) // using filter method will not get the values of multiplication
 //console.log(multiplyWithTwo);
 
-// trying with foreach
-const usingForEach  = myNum.forEach( (param, index) => {
-    console.log(`param ${param} | index : ${index}`);
+// trying with foreach, better use map instead of foreach
+const usingForEach  = myNum.forEach( (val, index) => {
+    console.log(`value is ${val} | index is: ${index}`);
     console.log(`modifyed ${myNum[index]}`);
-    return myNum[index] = param * 2 
+    return myNum[index] = val * 2
     
 })
 console.log(myNum);
--- playground.js
+++ playground.js
@@ -1,11 +1,59 @@
+// trying loog colors
+const randomColor = function() {
+    let hex = '012346789ABCDEF';
+    let color = '#';
+
+    for (let index = 0; index < 6; index++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    // console.log(color); // Print the color code
+    return color;
+};
+
+randomColor();
+
+
+// 
 const myArr = [1,2,3,4,5,6,7]
 
 // normal function
 myArr.filter( function(param){
     if(param > 2){
-        console.log(param);
+        // console.log(param);
     }
 })
+// usinf map
+const usingMap = myArr.map( (val) => (val+10) )
+// console.log(usingMap)
+
+
 // arrow function
 const usingArrow = myArr.filter( (param)=> param > 2)
 // console.log(usingArrow);
+
+
+// chacking this in normal function and arrow function
+const normalFun = {
+    name : 'Sumesh',
+    age : 15,
+    normal : function (){
+        console.log('I am normal function', this.name);
+        console.log(this);
+    }
+}
+// normalFun.normal();
+
+const arrowFun = {
+    name : 'Sali',
+    age : 25,
+    arrow () {
+        console.log('I am arrow function', this.name);
+        console.log(this);
+    } 
+        
+
+}
+// arrowFun.arrow()
+
+
