577862c4344a8cd610413ecf400094e1b8d2518d
Sumesh <sumeshmon2006@gmail.com>
2024-07-31 09:55:57 +0530

    dom

---
+++ projects/10-DOM/script.js
@@ -0,0 +1,16 @@
+// console.dir(window.document.location)
+
+const pageUrl = window.document.location.href
+// console.log(pageUrl);
+
+
+const h1InnerHtml = document.getElementById('h2').innerHTML
+const h1InnerText = document.getElementById('h2').innerText
+console.dir(h1InnerHtml)
+console.dir(h1InnerText)
+  
+const h2SubHeading = document.getElementsByClassName('h2-sub')
+console.dir(h2SubHeading)
+
+const para = document.getElementsByTagName('p')
+console.dir(para)
