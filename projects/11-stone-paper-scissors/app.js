c4f52fa28a5c7e24e6fa2f60fb058b590f453dd6
Sumesh <sumeshmon2006@gmail.com>
2024-08-18 09:24:38 +0530

    game

--- projects/11-stone-paper-scissors/app.js
+++ projects/11-stone-paper-scissors/app.js
@@ -1,12 +1,28 @@
-// taking user clicked value
 const userVal = document.querySelectorAll('.cta')
 
+// comuter generated
+const comGenerated = () => {
+    const options = ['stone', 'paper', 'scissor']
+    const randomIndex = Math.floor(Math.random() * 3)    
+    return options[randomIndex] 
+}
+
+  
+// comparison
+const playGame = (userChoice) => {
+    const choiceComputer = comGenerated()
+    console.log(`User choice is ${userChoice} and Computer choice is ${choiceComputer}`);
+    
+    
+}
+playGame()
+// taking user clicked value
 userVal.forEach ( (values) => {
     values.addEventListener ('click', () => {
-        const getIdValue = values.getAttribute('id')
-        console.log(getIdValue);
-        
-    })
-    
+        const userChoice = values.getAttribute('id')
+        //console.log(getIdValue);
+        playGame (userChoice)        
+    })   
     
 })
+
