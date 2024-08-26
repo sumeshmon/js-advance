11ab6fcad1f3c761f8bc1ccb9ae641e63960df8d
Sumesh <sumeshmon2006@gmail.com>
2024-08-26 09:40:42 +0530

    game rock,paper

--- projects/11-stone-paper-scissors/app.js
+++ projects/11-stone-paper-scissors/app.js
@@ -1,19 +1,22 @@
 const userVal = document.querySelectorAll('.cta')
-
+const display = document.querySelector('.result')
 // comuter generated
 const comGenerated = () => {
     const options = ['stone', 'paper', 'scissor']
     const randomIndex = Math.floor(Math.random() * 3)    
     return options[randomIndex] 
 }
-
-  
+const drawGame = () =>{
+    display.innerHTML = 'Match draw'
+}
 // comparison
 const playGame = (userChoice) => {
     const choiceComputer = comGenerated()
     console.log(`User choice is ${userChoice} and Computer choice is ${choiceComputer}`);
-    
-    
+    if (choiceComputer === userChoice){
+        drawGame()       
+    }
+   
 }
 playGame()
 // taking user clicked value
