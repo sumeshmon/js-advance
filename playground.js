f35fa56347b39318ad676ce7c43e68a198383487
Sumesh Sundaran <sumesh.sundaran@eqs.com>
2024-10-16 07:31:07 +0530

    promise resolve

--- playground.js
+++ playground.js
@@ -1,15 +1,10 @@
-const myArr = ['1','2','3','4','5','6','7','8']
+const promiseOne = new Promise ( (resolve,reject) => {
+    setTimeout(() => {
+        console.log(`Promise one is completed`);
+        resolve()
+    }, 1000);
+} )
 
-const testForLoop = myArr.forEach ( (ele) => {
-    if (ele >= 5 ){
-        // console.log(ele);
-        return console.log(ele);
-        
-    }
-    
-}) 
-// console.log(testForLoop);
-
-const testFilter = myArr.filter ( (elements) => elements >=5 )
-console.log(testFilter);
-
+promiseOne.then( () => {
+    console.log(`the task is completed`);
+} )
