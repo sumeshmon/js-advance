eaec916282427d35ef53ce4e746a3719de1acd18
ssundaran <sumesh.sundaran@eqs.com>
2025-02-01 12:26:36 +0530

    reverse string

---
+++ practise/reverses_string.js
@@ -0,0 +1,33 @@
+// split(''): Converts the string into an array of characters.
+// reverse(): Reverses the array.
+// join(''): Joins the array back into a string.
+function solution(str) {
+    // console.log(typeof str);
+    // const checkStr = str.split('')
+    // console.log(checkStr);
+    // checkStr.reverse()
+    // console.log(checkStr);
+    // const reverse = checkStr.join('')
+    // console.log(reverse);
+    return str.split('').reverse().join('')
+}
+  
+// console.log(solution('dlrow'));
+
+// using loop
+// function solution1(str){
+//     let revised = ''    
+//     for (i = str.length - 1; i >= 0; i--){
+//         revised += str[i]
+//     }
+//     return revised
+// }
+// console.log(solution1('dlrow'));
+
+// split(''): Converts the string into an array of characters.
+// reduce: Iterates through the array and builds the reversed string by prepending each character.
+function solution2(str){
+    return str.split('').reduce((reverse,char) => char + reverse, '');
+}
+
+console.log(solution2('dlrow'));
