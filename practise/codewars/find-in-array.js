8df3fc0c6cc738148719565842004db88668b523
ssundaran <sumesh.sundaran@eqs.com>
2025-02-03 09:23:39 +0530

    array filter

---
+++ practise/codewars/find-in-array.js
@@ -0,0 +1,29 @@
+const arraySheep = [true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true]
+
+function countSheeps (param){
+    // console.log(param.length);
+    
+    let count  = 0
+    // for (let i = 0; i < param.length; i++){
+    //     if(param[i] === true){
+    //         count++
+    //     }
+        
+    // }
+
+    // param.forEach(element => {
+    //     if (element){
+    //         count++
+    //     }
+    // });
+    // return count
+
+
+    return param.filter(Boolean).length;
+}
+console.log(countSheeps(arraySheep));
