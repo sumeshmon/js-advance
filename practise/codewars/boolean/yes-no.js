6826ad10917a031d4460f6c3c84e4cf454c09e20
ssundaran <sumesh.sundaran@eqs.com>
2025-02-25 09:43:22 +0530

    boolean

---
+++ practise/codewars/boolean/yes-no.js
@@ -0,0 +1,11 @@
+function boolToWord( bool ){
+    return bool ? `Yes` : `No`
+    // if (bool) {
+    //   return `Yes`
+    // }
+    // return `No`
+  }
+console.log(boolToWord(true));
+
+// problem
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
