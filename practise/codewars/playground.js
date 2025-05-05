380edbf075faefc1edca1378db35aae6b2184604
ssundaran <sumesh.sundaran@eqs.com>
2025-05-05 09:47:36 +0530

    math floor

--- practise/codewars/playground.js
+++ practise/codewars/playground.js
@@ -1,13 +1,6 @@
-function alphabetPosition(text) {
-    return text
-        .toLowerCase()
-        .split('')
-        .filter( char => /[a-z]/.test(char))
-        .map( char => char.charCodeAt(0) - 96)
+function cockroachSpeed(s) {
+    // let speed = Math.round((s * 100000) / 3600)
+    return Math.floor((s * 100000) / 3600)
 }
-input = "The sunset sets at twelve o' clock."
-console.log(alphabetPosition(input));
+console.log(cockroachSpeed(1.18));
 
-// problem
-// Input = "The sunset sets at twelve o' clock."
-// Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
--- practise/codewars/string/unicode_ASCII_calculator.js
+++ practise/codewars/string/unicode_ASCII_calculator.js
@@ -15,6 +15,13 @@ function alphabetPosition(text) {
 input = "The sunset sets at twelve o' clock."
 console.log(alphabetPosition(input));
 
+// using modern method
+// return text
+//         .toLowerCase()
+//         .split('')
+//         .filter( char => /[a-z]/.test(char))
+//         .map( char => char.charCodeAt(0) - 96)
+
 // problem
 // Input = "The sunset sets at twelve o' clock."
 // Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
