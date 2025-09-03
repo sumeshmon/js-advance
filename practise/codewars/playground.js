894160cb54d88a7cfd697ae55c71fee660ec004b
sumesh <sumesh.sundaran@eqs.com>
2025-09-03 11:26:47 +0530

    fix

--- practise/codewars/playground.js
+++ practise/codewars/playground.js
@@ -13,16 +13,27 @@ function addNewItem(newItem){
 }
 
 const newItem = {name : 'Tea', price : 12}
-
+console.log('Testing find()', menu.find (item => item.price));
 // console.log(addNewItem (newItem));
 
 function  placeOrder (itemName) {
+    
+    
    const foundItem = menu.find(item => item.name === itemName);
     if( !itemName){
             throw new Error ('not matching')
     }
     cashInCounter += foundItem.price
+
+    const order = {
+        name : itemName,
+        status : 'Ordered'
+    }
+    orderQueue.push(order)
+    return order
 }
 
-const myOrder = placeOrder("Biriyani");
-console.log(cashInCounter)
+const myOrder = placeOrder("Meals");
+console.log("Total cash in order rs: ",cashInCounter)
+
+console.log("Order Queue:", orderQueue);
